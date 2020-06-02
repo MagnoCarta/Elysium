c687ccb9c0e8e50b331a0f0fb5823d38fcc3f5bb
Rudigus <rodrigo.matos9@hotmail.com>
2020-06-02 09:39:58 -0300

    adding initial project files

---
+++ GameChallenge/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GameChallenge
+//
+//  Created by Rodrigo Matos Aguiar on 02/06/20.
+//  Copyright © 2020 Placeholder. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
