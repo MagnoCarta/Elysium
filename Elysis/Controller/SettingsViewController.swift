cf2f52cdd0cfa2e490cb5795eef640ef9af121e0
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-11 11:53:32 -0300

    Adicionado botao de voltar e label de opcao

--- Elysis/Controller/SettingsViewController.swift
+++ Elysis/Controller/SettingsViewController.swift
@@ -10,17 +10,32 @@ import Cocoa
 
 class SettingsViewController: NSViewController {
 
-    var backButton = NSButton(frame: NSRect(x: 50, y: 50, width: 100, height: 100))
+    var backButton: NSButton = {
+        let backbutton = NSButton()
+        backbutton.title = "Back"
+        return backbutton
+    }()
+    
+    var optionsTitle: NSText = {
+        let options = NSText()
+        options.string = "Options"
+        options.font = NSFont(name: "AppleSystemUIFont", size: 30)
+        options.alignment = .center
+        return options
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(backButton)
-        backButton.setFrameOrigin(NSPoint(x: self.view.frame.minX + 150, y: self.view.frame.maxY - 150))
+        
+//        backButton.setFrameOrigin(NSPoint(x: self.view.frame.minX + 150, y: self.view.frame.maxY - 150))
+        setupButton()
+        setupOptionsTitle()
         backButton.action = #selector(didTapBackButton(_:))
+        
     }
     
     override func loadView() {
-       self.view = NSView(frame: NSRect(x: NSScreen.main!.frame.minX, y: NSScreen.main!.frame.minY, width: NSScreen.main!.frame.width, height: NSScreen.main!.frame.height))
+        self.view = NSView(frame: NSRect(x: NSScreen.main!.frame.minX, y: NSScreen.main!.frame.minY, width: NSScreen.main!.frame.width, height: NSScreen.main!.frame.height))
     }
     
     @objc  func didTapBackButton(_ Button: AnyObject){
@@ -28,5 +43,30 @@ class SettingsViewController: NSViewController {
         //MODEL FULLSCREEN
         
         self.view.window!.contentViewController = ViewController()
+    }
+    
+    func setupButton() {
+        self.view.addSubview(backButton)
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.backButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100),
+            self.backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: -100),
+            self.backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100),
+            self.backButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -1100)
+        ])
+    }
+    
+    func setupOptionsTitle() {
+        self.view.addSubview(optionsTitle)
+        optionsTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.optionsTitle.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100),
+            self.optionsTitle.topAnchor.constraint(equalTo: self.view.topAnchor, constant: ((100*self.view.frame.height)/self.view.frame.height)),
+            self.optionsTitle.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 200),
+            self.optionsTitle.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -100)
+        ])
     }
 }
