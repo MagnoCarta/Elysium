f91a34f5d5cff075bdca663c16f25cca5c57263d
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-06-13 21:52:00 -0300

    Primeiros testes

--- ElysisTests/ElysisTests.swift
+++ ElysisTests/ElysisTests.swift
@@ -23,6 +23,17 @@ class ElysisTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    //Verifica se a struct Interaction esta aceitando valores corretamente
+    func testInteractionStruct() {
+        let interaction = Interaction(playerAnswer: "Happy", answerPolarity: .positive)
+        XCTAssertEqual(interaction.playerAnswer, "Happy", "Value of playerAnswer must be Happy")
+        XCTAssertEqual(interaction.answerPolarity, Polarity(rawValue: "positive"), "Value of answerPolarity must be positive")
+    }
+    
+    func testGameStateClass() {
+        
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
