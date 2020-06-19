93fa2faf8909c5562d4511a8d55142138e2298de
Rudigus <rodrigo.matos9@hotmail.com>
2020-06-18 21:50:04 -0300

    adding reset button and starting at min and max slider labels

--- Elysis/Controller/SettingsViewController.swift
+++ Elysis/Controller/SettingsViewController.swift
@@ -8,6 +8,11 @@
 
 import Cocoa
 
+enum SliderValue {
+    case max
+    case min
+}
+
 class SettingsViewController: NSViewController {
     
     //MARK: Views and Controls Initialization
@@ -86,6 +91,24 @@ class SettingsViewController: NSViewController {
         return slider
     }()
     
+    var textSizeMinLabel: NSText = {
+        let label = NSText()
+        label.string = "A"
+        label.font = NSFont(name: "AppleSystemUIFont", size: 16)
+        label.isEditable = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
+    var textSizeMaxLabel: NSText = {
+        let label = NSText()
+        label.string = "A"
+        label.font = NSFont(name: "AppleSystemUIFont", size: 48)
+        label.isEditable = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
     var textSpeedLabel: NSText = {
         let label = NSText()
         label.string = "Text Speed"
@@ -104,13 +127,39 @@ class SettingsViewController: NSViewController {
         slider.doubleValue = UserDefaults.standard.double(forKey: "textSpeed")
         slider.alignment = .center
         slider.action = #selector(didChangeSlider(_:))
+        slider.numberOfTickMarks = 5
         return slider
+    }()
+    
+    var textSpeedMinLabel: NSText = {
+        let label = NSText()
+        label.string = "16px"
+        label.font = NSFont(name: "AppleSystemUIFont", size: 16)
+        label.isEditable = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
+    var textSpeedMaxLabel: NSText = {
+        let label = NSText()
+        label.string = "48px"
+        label.font = NSFont(name: "AppleSystemUIFont", size: 16)
+        label.isEditable = false
+        label.backgroundColor = .clear
+        return label
     }()
 
     var doneButton: NSButton = {
         let button = NSButton()
         button.title = "Done"
         button.action = #selector(didTapDoneButton)
+        return button
+    }()
+    
+    var resetButton: NSButton = {
+        let button = NSButton()
+        button.title = "Reset"
+        button.action = #selector(didTapResetButton)
         return button
     }()
     
@@ -124,14 +173,17 @@ class SettingsViewController: NSViewController {
         super.viewDidLoad()
         setupOptionsTitle()
         setup(label: bgmVolumeLabel, relativeTo: optionsTitle, additionalMargin: 50)
-        setup(control: bgmVolumeSlider, for: bgmVolumeLabel)
+        setup(slider: bgmVolumeSlider, for: bgmVolumeLabel)
         setup(label: sfxVolumeLabel, relativeTo: bgmVolumeLabel)
-        setup(control: sfxVolumeSlider, for: sfxVolumeLabel)
+        setup(slider: sfxVolumeSlider, for: sfxVolumeLabel)
         setup(label: textSizeLabel, relativeTo: sfxVolumeLabel)
-        setup(control: textSizeSlider, for: textSizeLabel)
+        setup(slider: textSizeSlider, for: textSizeLabel)
+        setup(label: textSizeMinLabel, for: .min, relativeTo: textSizeSlider)
+        setup(label: textSizeMaxLabel, for: .max, relativeTo: textSizeSlider)
         setup(label: textSpeedLabel, relativeTo: textSizeLabel)
-        setup(control: textSpeedSlider, for: textSpeedLabel)
-        setup(doneButton: doneButton, relativeTo: textSpeedLabel)
+        setup(slider: textSpeedSlider, for: textSpeedLabel)
+        setup(button: doneButton, relativeTo: textSpeedLabel, trailingConstant: 100)
+        setup(button: resetButton, relativeTo: textSpeedLabel, trailingConstant: 300)
     }
     
     //MARK: Actions
@@ -145,6 +197,13 @@ class SettingsViewController: NSViewController {
         self.view.window!.contentViewController = ViewController()
     }
     
+    @objc func didTapResetButton() {
+        resetValue(bgmVolumeSlider, forKey: "bgmVolume")
+        resetValue(sfxVolumeSlider, forKey: "sfxVolume")
+        resetValue(textSizeSlider, forKey: "textSize")
+        resetValue(textSpeedSlider, forKey: "textSpeed")
+    }
+    
     @objc func didChangeSlider(_ slider: NSSlider) {
 //        print(slider.doubleValue)
     }
@@ -152,6 +211,13 @@ class SettingsViewController: NSViewController {
     @objc func didChangeIntSlider(_ slider: NSSlider) {
 //        print(slider.doubleValue)
         slider.doubleValue = round(slider.doubleValue)
+    }
+    
+    //MARK: Other
+    
+    func resetValue(_ slider: NSSlider, forKey key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
+        slider.doubleValue = UserDefaults.standard.double(forKey: key)
     }
     
     //MARK: Setup
@@ -173,30 +239,53 @@ class SettingsViewController: NSViewController {
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: referenceLabel.bottomAnchor, constant: 30 + additionalMargin),
             label.trailingAnchor.constraint(equalTo: optionsTitle.leadingAnchor, constant: 100),
-            label.heightAnchor.constraint(equalToConstant: view.frame.height * 0.05),
+            label.heightAnchor.constraint(equalToConstant: view.frame.height * 0.06),
             label.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
         ])
     }
     
-    func setup(control: NSControl, for label: NSText) {
-        self.view.addSubview(control)
-        control.translatesAutoresizingMaskIntoConstraints = false
+    func setup(slider: NSSlider, for label: NSText) {
+        self.view.addSubview(slider)
+        slider.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            control.topAnchor.constraint(equalTo: label.topAnchor),
-            control.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 100),
-            control.heightAnchor.constraint(equalToConstant: view.frame.height * 0.05),
-            control.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
+            slider.topAnchor.constraint(equalTo: label.topAnchor, constant: -25),
+            slider.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 100),
+            slider.heightAnchor.constraint(equalToConstant: view.frame.height * 0.10),
+            slider.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
         ])
     }
     
-    func setup(doneButton: NSButton, relativeTo label: NSText) {
-        self.view.addSubview(doneButton)
-        doneButton.translatesAutoresizingMaskIntoConstraints = false
+    func setup(label: NSText, for value: SliderValue, relativeTo slider: NSSlider) {
+        self.view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        switch value {
+        case SliderValue.min:
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: -20),
+                label.leadingAnchor.constraint(equalTo: slider.leadingAnchor, constant: -5),
+                label.heightAnchor.constraint(equalToConstant: view.frame.height * 0.10),
+                label.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
+            ])
+            break
+        case SliderValue.max:
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: -20),
+                label.leadingAnchor.constraint(equalTo: slider.trailingAnchor, constant: -10),
+                label.heightAnchor.constraint(equalToConstant: view.frame.height * 0.10),
+                label.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
+            ])
+            break
+        }
+    }
+    
+    func setup(button: NSButton, relativeTo label: NSText, trailingConstant: CGFloat = 0) {
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.doneButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
-            self.doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            self.doneButton.heightAnchor.constraint(equalToConstant: view.frame.height * 0.05),
-            self.doneButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.2)
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
+            button.trailingAnchor.constraint(equalTo: optionsTitle.leadingAnchor, constant: trailingConstant),
+            button.heightAnchor.constraint(equalToConstant: view.frame.height * 0.05),
+            button.widthAnchor.constraint(equalToConstant: view.frame.width * 0.1)
         ])
     }
 }
